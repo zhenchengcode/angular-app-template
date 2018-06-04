f6802d33487cf21fefc198b401ecff56f5b00e2d
zhenchengcode <amadeus.zhencheng@gmail.com>
2018-06-04 13:59:33 -0700

    make spacing better

--- src/app/description/description.component.ts
+++ src/app/description/description.component.ts
@@ -23,7 +23,7 @@ export class DescriptionComponent implements OnInit, AfterViewInit {
 		token_selected_label: 'color',
 	}
 
-	description = new Description('1', 'orange truck carries orange and another zip', ['orange', 'carries', 'zip'])
+	description = new Description('1', 'orange truck carries orange and another zip for a zebra', ['orange', 'carries', 'zip', 'zebra'])
 
 
   // description:Description = {
