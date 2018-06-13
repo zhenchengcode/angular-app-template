93d980fd02f7334b5584fcf381095bc13340206d
zhenchengcode <amadeus.zhencheng@gmail.com>
2018-06-13 14:58:26 -0700

    remove redundant code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,7 @@ import { InMemoryDataService }  from './in-memory-data.service';
 import { AppRoutingModule }     from './app-routing.module';
 
 import { AppComponent }         from './app.component';
-import { DashboardComponent }   from './dashboard/dashboard.component';
-import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
-import { HeroesComponent }      from './heroes/heroes.component';
-import { HeroSearchComponent }  from './hero-search/hero-search.component';
-import { MessagesComponent }    from './messages/messages.component';
+
 import { DescriptionComponent } from './description/description.component';
 import { TokenComponent } from './token/token.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -42,11 +38,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   declarations: [
     AppComponent,
-    DashboardComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    MessagesComponent,
-    HeroSearchComponent,
     DescriptionComponent,
     TokenComponent,
 
