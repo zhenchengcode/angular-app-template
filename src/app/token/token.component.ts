637b3c8dd682efbc1bbe8cb15448ec6a75e65c7c
zhenchengcode <amadeus.zhencheng@gmail.com>
2018-06-14 14:10:49 -0700

    try dedup keyup events

--- src/app/token/token.component.ts
+++ src/app/token/token.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, HostListener} from '@angular/core';
 import { Description } from '../description/description';
 import { Token } from './token';
 
@@ -8,10 +8,12 @@ import { Token } from './token';
   selector: 'app-token',
   templateUrl: './token.component.html',
   styleUrls: ['./token.component.css'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  // host: {'(window:keyup)': 'hotkeys($event)'}
 })
 export class TokenComponent implements OnInit {
   @Input() displayToken: Token;
+  eventCounter: number = 1;
 
   attributes = [
     {value: 'capacity-0', viewValue: 'Capacity Blah Blah Blah'},
@@ -20,6 +22,23 @@ export class TokenComponent implements OnInit {
   ];
 
   selected_item = this.attributes[0].value;
+
+  // hotkeys(event){
+  //   if (event.keyCode == 65 && event.keyCode == 65){
+  //     this.showMessage();
+  //   }
+  // }
+
+  @HostListener('window:keyup', ['$event'])
+  showMessage(event: KeyboardEvent){
+    console.log('captured')
+    this.eventCounter += 1;
+    console.log('after increase')
+    console.log(this.eventCounter)
+    // if (event.keyCode === 65 && this.eventCounter==1) {
+    //   console.log('Hotkey Test');
+    // }
+  }
 
 
   ngOnInit() {
