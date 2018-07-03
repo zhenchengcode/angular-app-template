acb675cbcabfb68cd310e69bdd9caebe29cb38b1
zhenchengcode <amadeus.zhencheng@gmail.com>
2018-07-03 10:58:46 -0700

    cleanup code

--- src/app/description/description.component.ts
+++ src/app/description/description.component.ts
@@ -26,43 +26,23 @@ export class DescriptionComponent implements OnInit {
 
 	description = new Description('1', 'orange truck carries orange and another zip for a zebra blueberry truck carries melon and another ship for a fish', ['orange truck', 'zip', 'zebra', 'melon', 'ship'])
 
-
-  // description:Description = {
-  // 	item_id: 1,
-  // 	content: 'orange truck carries orange',
- 	//   input_tokens: [this.token.token_text]
-  // }
-
-
-
-
   token_segs = this.description.parse_description(this.description.content, this.description.tokens);
   displayTokens = this.token_segs;
-  // segs = [
-  //   {value:'apple', id:0},
-  //   {value:'pear', id:1},
-  //   {value:'pineapple', id:2},
-  // ];
+
 
   eventCounter: number = 1;
   prevTime: number = NaN;// last time a key is pressed
   keyStrokes : Array<string> = new Array();
   @HostListener('window:keyup', ['$event']) // selector is the 'app-token', so if there are 10 tokens, showMessage will get called 10 times on single keyup event
-  showMessage(event: KeyboardEvent){
+  captureKeyStroke(event: KeyboardEvent){
 
-    // when a special key is pressed, print out keyStrokes
-    // console.log(Date.now() - this.prevTime)
+    if( !isNaN(this.prevTime) && event.keyCode === 13) {
 
-    // && (Date.now() - this.prevTime)/1000 > 1
-    if( !isNaN(this.prevTime) && event.keyCode === 13) { // &&  event.keyCode===188
-      // console.log(this.keyStrokes);
       let keyStrokes:string = this.keyStrokes.join('');
       keyStrokes = keyStrokes.toLowerCase();
 
-      console.log(keyStrokes); // need to put here, after the following loop won't work
       let dropDownElement = document.getElementsByClassName(keyStrokes)[0];
-      
-      console.log(dropDownElement)
+
       if (dropDownElement!==null && dropDownElement!==undefined) {
         dropDownElement.click();
       }
@@ -77,26 +57,13 @@ export class DescriptionComponent implements OnInit {
       this.prevTime = Date.now();
     }
 
-
-
-    console.log(this.eventCounter);
     this.eventCounter += 1;
 
   }
 
-  /*
-  * Send keystrokes captured */
-  // @HostListener('window:keyup', ['$event'])
-  // sendKeystrokes(event: KeyboardEvent){
-  //   // currentTime - prevTime > interval, send key strokes, clear this.prevTime and this.keyStrokes
-  //
-  // }
-
-
 
 
   ngOnInit() {
-    console.log(this.description.tokens)
   }
 
 }
